f8e76020870edc0aec585ceea51766081ff57e62
Arnaud Tiam <arnaudtiam50@gmail.com>
2024-04-13 23:03:52 -0500

    Update style.css, cocktail.js, scripts.js, tailwind.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,13 +10,14 @@ const options = {
   const movieListUrl = 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&with_genres=10749%2C%2035%20%7C%2018%20%7C%2010402&with_original_language=en&without_genres=28%2C%2012%2C%2016%2C%2080%2C%2099%2C%2010751%2C%2014%2C%2036%2C%2027%2C%209648%2C%20878%2C%2010770%2C%2053%2C%2010752%2C%2037'
   // const titleEl = document.querySelector("#movieTitle");
   const posterEl = document.querySelector("#moviePoster");
+
   const descriptionEl = document.querySelector("p");
   const moodSelectEl = document.querySelector("#mood-select");
   const buttonEl = document.querySelector("button");
 
   const movieSectionEl = document.querySelector("#movie-section");
   const movieTitleEl = document.querySelector("#movie-title");
-  const movieDescriptionSectionEl = document.querySelector("#movie-description_section");
+  // const movieDescriptionSectionEl = document.querySelector("#movie-description_section");
 
   function displayMovie(movieData){
     // titleEl.textContent = movieData.title;
@@ -24,25 +25,40 @@ const options = {
     movieTitleEl.textContent = movieData.title;
 
     //Create image element and append to the movie section.
+
+    const posterContainer = document.createElement("div");
+    posterContainer.classList.add(`border-black`);
+    const posterTitleEl = document.createAttribute("h4");
     const moviePosterEl = document.createElement("img");
     moviePosterEl.setAttribute("src", "https://media.themoviedb.org/t/p/w300_and_h450_bestv2" + movieData.poster_path);
+    // moviePosterEl.classList.add(`w-20`, `h-21`);
     // We should also add alt element.
 
+    posterTitleEl.textContent = "Movie Posture";
+
     //Append the image to its container.
-    movieSectionEl.append(moviePosterEl);
+    posterContainer.appendChild(moviePosterEl, moviePosterEl);
+
+    movieSectionEl.appendChild(posterContainer);
 
     // Create element for description
-    const movieDescriptionEl = document.createElement("p");
+    
+       const movieDescriptionContentEl = document.createElement("p");
     // Add the description of the movie.
-    movieDescriptionEl.textContent = movieData.overview;
+
+    movieDescriptionContentEl.textContent = movieData.overview;
     
 
     // Append
-    movieDescriptionSectionEl.append(movieDescriptionEl);
+  
+    // descriptionContainer.append(movieDescriptionEl);
+    movieSectionEl.appendChild(movieDescriptionContentEl);
+    
 
     // titleEl.textContent = movieData.title;
     // posterEl.setAttribute("src", "https://media.themoviedb.org/t/p/w300_and_h450_bestv2" + movieData.poster_path);
     // descriptionEl.textContent = movieData.overview;
+
 
   }
 
